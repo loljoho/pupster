bf97a2a7533eb28e98dad8bf2ad59c918ae0371b
loljoho <loljoho@users.noreply.github.com>
2019-12-02 17:00:28 -0500

    Use getRandomDog for Discover page component

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import API from '../utils/API';
 
-function Discover() {
-  return (
-    <>
-      <h1>Discover Pupster!</h1>
-    </>
-  );
+class Discover extends Component {
+  
+  state = {
+    image: ''
+  };
+
+  componentDidMount() {
+    this.loadDog();
+  }
+
+  loadDog = () => {
+    API.getRandomDog()
+      .then(res => {
+          console.log(res);
+          this.setState({
+            image: res.data.message
+          });
+        }
+      )
+      .catch(err => console.warn(err));
+  }
+
+
+  render() {
+    return (
+      <>
+        <h1>Discover Pupster!</h1>
+        <img src={this.state.image} />
+      </>
+    );
+  }
+
 }
 
 export default Discover;
